01351207d72751cd1750f79d17082321bc57f8c8
Rishabh Singh <rishabhsingh119@gmail.com>
2026-08-29 19:17:31 -0400

    Update ddl.sql

--- scripts/silver/ddl.sql
+++ scripts/silver/ddl.sql
@@ -12,12 +12,14 @@ CREATE TABLE silver.crm_cust_info (
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
     prd_id INT,
+	cat_id VARCHAR(50),
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
     prd_cost INT,
     prd_line VARCHAR(50),
     prd_start_dt DATE,
-    prd_end_dt DATE
+    prd_end_dt DATE,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 DROP TABLE IF EXISTS silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details (
