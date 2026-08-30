0d92337edf6a52cd8ad271f3ee2c4001f4f923ae
Rishabh Singh <rishabhsingh119@gmail.com>
2026-08-30 14:00:19 -0400

    Create product_view.sql

---
+++ scripts/gold/product_view.sql
@@ -0,0 +1,17 @@
+create view gold.dim_products as 
+select
+row_number() over (partition by pn.prd_start_dt, pn.prd_key) as product_key ,
+pn.prd_id as product_id ,
+pn.prd_key as product_number,
+pn.prd_nm as product_name,
+pn.cat_id as category_id,
+pc.cat as category,
+pc.subcat as subcategory,
+pc.maintenance,
+pn.prd_cost as cost,
+pn.prd_line as product_line,
+pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc
+on pn.cat_id = pc.id
+where prd_end_dt is null 
