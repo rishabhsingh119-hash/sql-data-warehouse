64322c200a59ba01fbd3f1f84ca8c1fed468d624
Rishabh Singh <rishabhsingh119@gmail.com>
2026-08-30 11:52:08 -0400

    Create erp_loc_a101.sql

---
+++ scripts/silver/erp_loc_a101.sql
@@ -0,0 +1,9 @@
+insert into silver.erp_loc_a101 (cid, cntry)
+select 
+    replace(cid, '-', '') as cid,
+    case when trim(cntry) = 'DE' then 'Germany'
+         when trim(cntry) in ('US', 'USA') then 'United States'
+         when trim(cntry) = '' or cntry is null then 'n/a'
+         else trim(cntry)
+         end as cntry 
+from bronze.erp_loc_a101
