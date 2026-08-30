285ca29d571106a763c9dbf0306d830e23c5fa4c
Rishabh Singh <rishabhsingh119@gmail.com>
2026-08-30 11:25:05 -0400

    Update ddl.sql

--- scripts/silver/ddl.sql
+++ scripts/silver/ddl.sql
@@ -26,12 +26,13 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
-    sls_price INT
+    sls_price INT,
+    dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
